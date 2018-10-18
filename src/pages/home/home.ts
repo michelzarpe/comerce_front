baaf1675a90d7edd811c6650ecb487f8bc3ebf7a
michelzarpe <michelzarpe@gmail.com>
2018-10-18 10:27:01 -0300

    Navegação

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -8,8 +8,18 @@ import { NavController, IonicPage } from 'ionic-angular';
 })
 export class HomePage {
 
+  // se tu quiser injetar alguma variavel é só colocar no construtor da classe
   constructor(public navCtrl: NavController) {
 
+  }
+
+  public login(){
+    //navegar pagina - colocar entre aspas por causa do layzi
+    //push é usado para empilhar e é possivel voltar a pagina anterior
+    //this.navCtrl.push('CategoriasPage');
+  
+    //setRoot -> nao empilha as paginas
+    this.navCtrl.setRoot('CategoriasPage');
   }
 
 }
