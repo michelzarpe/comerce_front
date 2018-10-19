dc3422c867ab8658c788aa102330dc02a00f22c8
michelzarpe <michelzarpe@gmail.com>
2018-10-19 10:49:50 -0300

    Criado um tratamento padrão para errod diferentes de 401 e 403

--- src/interceptors/error.interceptors.ts
+++ src/interceptors/error.interceptors.ts
@@ -2,14 +2,14 @@ import { Injectable } from "@angular/core";
 import { HttpInterceptor, HTTP_INTERCEPTORS, HttpEvent, HttpHandler, HttpRequest } from "@angular/common/http";
 import { Observable } from "rxjs/Rx";
 import { StorageService } from "../services/storage.service";
+import { AlertController } from "ionic-angular";
 
 
 //classe para interceptar os erros
 @Injectable()
 export class ErrorInterceptor implements HttpInterceptor{
     
-    constructor(public sotrage: StorageService){
-
+    constructor(public sotrage: StorageService, public alertController: AlertController){
     }
 
     intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
@@ -28,15 +28,38 @@ export class ErrorInterceptor implements HttpInterceptor{
 
             switch(errorObj.status){
                 case 403: this.handle403(); break;
+                case 401: this.handle401(); break;
+                default: this.handleDefaultError(errorObj);
             }
            return Observable.throw(errorObj);
        }) as any;
+    }
+    handleDefaultError(errorObj) {
+        this.alertPadrao(errorObj.status,errorObj.error,errorObj.mensage);
     }
 
     //forçar a limpeza do localStorage
     handle403(){
         this.sotrage.setLocalUser(null);
     }
+
+    //mensagem de erro para usuario e senha
+    handle401(){
+        this.alertPadrao('401','Falaha de autenticação','E-mail ou Senha incorretos');
+    }
+
+
+    alertPadrao(status: string, error: string, mensagem: string){
+        let alert = this.alertController.create({
+            title: 'Erro '+status+':'+error,
+            message: mensagem,
+            enableBackdropDismiss: false,
+            buttons: [{text:'Ok'}]
+        });
+        alert.present(); //apresenta o alert
+    }
+
+    
 }
 
 
